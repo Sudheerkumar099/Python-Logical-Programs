1aa72aa0ba15e0cf64d973bc0f4866ea096a5eab
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-18 15:32:07 +0530

    setup:[sudheer] Replacing the string

---
+++ ReplaceString.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+while(len(name)<=2):
+   print("Name must be more than 2 characters.")
+   name=input("Enter your name: ") 
+print("Hello, ",name," How are you?")
+  
