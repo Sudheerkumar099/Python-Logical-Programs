3cf586ce2be72ad414bb495299ed98c665fea08b
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-18 17:43:31 +0530

    ProblemSolved : [sudheer] Harmonic Number

---
+++ Harmonic_Number.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a positive integer: "))
+while(n<0):
+    n = int(input("Please enter a positive integer: "))
+
+hn=0
+for i in range(1,n+1,1):
+    hn=hn+1/i
+        
+print("Harmonic Number is:",hn)
