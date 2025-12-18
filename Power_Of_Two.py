2fcf2f11fcb39633e93dd97f50ad51638c404b5c
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-18 17:26:53 +0530

    Taking input again if input is wrong

--- Power_Of_Two.py
+++ Power_Of_Two.py
@@ -1,7 +1,12 @@
 n= int(input("Enter a number: "))
-if(n<=0 or n<31):
+while(n<0 or n>30):
+    n= int(input("Enter a number between 0 and 30: "))
+    
+if(n>=0 and  n<31):
     for i in range(0,31,1):
         power=2**i
         if(power<=2**n):
          print(f"2  power {i} = {power}")
+
+
          
