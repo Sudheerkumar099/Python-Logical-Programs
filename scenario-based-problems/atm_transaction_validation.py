4d441305f20f7f14238dbe1bf7e0ef73497809d2
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-19 20:07:02 +0530

    .

---
+++ scenario-based-problems/atm_transaction_validation.py
@@ -0,0 +1,13 @@
+balance = int(input("Enter the balance : "))
+n = input("Enter the number of transactions: ")
+lst = []
+for i in range(int(n)):
+    amount = int(input("Enter the amount : "))
+    if amount <= balance and amount % 100 == 0:
+        balance = balance - amount
+        lst.append("SUCCESS")
+    else:
+        lst.append("FAILED")
+for status in lst:
+    print(status)
+print(f"Remaining balance: {balance}")
