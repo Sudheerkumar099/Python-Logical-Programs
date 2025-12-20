89acf61f0a7943fb7b9a7ba932b993d1a83c999a
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-20 09:42:57 +0530

    ProblemSolved : [sudheer] Solved Mobile Battery Drain Simulator Problem

---
+++ scenario-based-problems/battery_drain_simulator.py
@@ -0,0 +1,9 @@
+battery_percent = 100
+minutes = 0
+drain_per_minute = int(input("Enter the battery percentage drain per minute: "))
+
+while battery_percent>0 and drain_per_minute<=battery_percent:
+    battery_percent = battery_percent - drain_per_minute
+    minutes = minutes + 1
+print(battery_percent)
+print(minutes)
